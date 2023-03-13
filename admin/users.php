b361bf5309afddca472d4bb382ba6c29a996d77b
SOPHIE ANGELA VILLALBA <72681828+sophie-angela-villalba@users.noreply.github.com>
2023-03-14 01:22:20 +0800

    nav and css update sa users

--- admin/users.php
+++ admin/users.php
@@ -1,10 +1,21 @@
+<header class="row-container">
+ 
+	<button id="menu-btn" class="menu">
+		<span class="material-symbols-outlined">menu</span>
+	</button>
+ 
+</header>
+ 
+
+
+
 <div class="container">
     <aside>
       <div class="top">
 
         <!--top btn-->
         <div class="logo">
-          <img src="./assets/images/pup.png" alt="">
+          <img src="../assets/PUP-Logo.png" alt="">
           <h2>PUP<span class="danger">QS</span></h2>
         </div>
 
@@ -137,23 +148,7 @@
     </main>
 <!--------------------------END OF MAIN--------------->
 
-    <div class="right">
-      <div class="top">
-        <button id="menu-btn">
-          <span class="material-symbols-outlined">menu</span>
-        </button>
-        <div class="profile">
-          <div class="info">
-            <p>Hey, <b>Flor</b></p>
-            <small class="text-muted">Admin</small>
-          </div>
-          <div class="profile-photo">
-            <span class="material-symbols-outlined">account_circle</span>
-          </div>
-        </div>
-      </div>
-      <!--------------------------END OF TOP--------------->
-    </div>
+
 
   </div>
 	
@@ -171,12 +166,15 @@
 	const closeBtn = document.querySelector("#close-btn");
 
 	menuBtn.addEventListener('click',()=>{
-		sideMenu.style.display = 'block';
-	})
+    sideMenu.style.display = 'block';
+    menuBtn.style.display = 'none';
+  })
 
-	closeBtn.addEventListener('click', ()=>{
-	sideMenu.style.display = 'none';
-	})
+  closeBtn.addEventListener('click', ()=>{
+  sideMenu.style.display = 'none';
+  menuBtn.style.display = 'block';
+  })
+
 	$('#manage-user').submit(function(e){
 		e.preventDefault();
 		$.ajax({
