6af071401c0538929e0a29bc3f2166653637aa3d
robinhud07 <55908416+robinhud07@users.noreply.github.com>
2023-03-15 16:04:54 +0800

    home placeholder

--- home.php
+++ home.php
@@ -58,10 +58,11 @@
     
             <h2>Select Transaction Queue</h2>
             <h4>Select a transaction queue serving display.</h4>
-
+            
             <?php 
                 $trans = $conn->query("SELECT * FROM transactions where status = 1 order by name asc");
-                while($row=$trans->fetch_assoc()):
+                if($trans->num_rows > 0):
+                    while($row=$trans->fetch_assoc()):
             ?>
 
             <a href="display.php?id=<?php echo $row['id'] ?>">
@@ -75,6 +76,12 @@
             </a>
             
             <?php endwhile; ?>
+            <?php else: ?>
+                <div class="placeholder-grid">
+                <h1>No Available Transactions at the Moment.</h1>
+                <span>Sorry! You can check and come back later.</span>
+                </div>
+            <?php endif; ?>
             
         </div>
 	</main>
